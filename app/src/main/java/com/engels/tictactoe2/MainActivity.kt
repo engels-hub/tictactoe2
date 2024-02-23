fbd141d599ea51733c052dd2839121c86709b17f
engels-hub <artjompapsh2003@gmail.com>
2024-02-23 11:45:30 +0200

    Fixed support for synth buttons, added jcenter()

--- app/src/main/java/com/engels/tictactoe2/MainActivity.kt
+++ app/src/main/java/com/engels/tictactoe2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.engels.tictactoe2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonPvP: Button = findViewById(R.id.button_pve);
+        buttonPvP.setOnClickListener {
+            val intent = Intent(this, PvCActivity::class.java)
+            startActivity(intent)
+        }
+        val buttonPvE: Button = findViewById(R.id.button_pvp);
+        buttonPvE.setOnClickListener {
+            val intent = Intent(this, PvPActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
